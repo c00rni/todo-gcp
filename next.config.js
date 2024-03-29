f6558f176f7b75745b5af28cfd2797243729f05a
corni <gmigan.a@gmail.com>
2024-03-29 01:53:23 +0100

    fix: Authorize every domaine
    Autorize communication with firebase

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
+        hostname: '**',
         port: '',
         pathname: '/**',
       },
